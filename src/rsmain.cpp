6e45fa3dbac7150e8013c2fb7ff11684c6ed9257
Stephen Paine <16stevetp@gmail.com>
2018-04-17 15:19:23 +0200

    Update version number
    Version number updated to reflect change to output

--- src/rsmain.cpp
+++ src/rsmain.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 {
   rsDebug::printf(rsDebug::RS_CRITICAL, "/------------------------------------------------\\\n");
   rsDebug::printf(rsDebug::RS_CRITICAL, "| FERS - The Flexible Extensible Radar Simulator |\n");
-  rsDebug::printf(rsDebug::RS_CRITICAL, "| Version 0.24                                   |\n");
+  rsDebug::printf(rsDebug::RS_CRITICAL, "| Version 0.25                                   |\n");
   rsDebug::printf(rsDebug::RS_CRITICAL, "\\------------------------------------------------/\n\n");
 
   if (argc != 2 || !strncmp(argv[1], "--help", 6))
