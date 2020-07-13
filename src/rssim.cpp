3ccbd1324336216f3780cf9d3b4e332ea7294017
OnePlus101 <43749523+OnePlus101@users.noreply.github.com>
2020-07-13 15:09:02 +0200

    Update rssim.cpp
    Should the phase equation not be the same for both SolveRE and SolveREDirect? Can someone explain the discrepancy?

--- src/rssim.cpp
+++ src/rssim.cpp
@@ -76,7 +76,7 @@ void SolveRE(const Transmitter *trans, const Receiver *recv, const Target *targ,
     results.power *= trans->MultipathDualFactor();
   //Step 3, calculate phase shift
   //See "Phase Delay Equation" in doc/equations/equations.tex
-  results.phase = -results.delay*2*M_PI*wave->GetCarrier();
+  results.phase = fmod(results.delay*2*M_PI*wave->GetCarrier(), 2*M_PI);
   //Step 4, calculate doppler shift
   //Calculate positions at the end of the pulse
   Vec3 trpos_end = trans->GetPosition(time+length);
