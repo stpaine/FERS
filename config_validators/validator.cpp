188dca0dfff380e02f263b89661e916287abb758
Grabt234 <gnorrie12@gmail.com>
2024-03-30 23:15:23 +0200

    Chore: more refactoring of parser

--- config_validators/validator.cpp
+++ config_validators/validator.cpp
@@ -29,9 +29,10 @@ void printUsage(const std::string &programName)
 {
     std::cout << "Usage: " << programName << " [options] <arg1> <arg2> ...\n"
               << "Options:\n"
-              << "  -h, --help    Show this help message\n"
-              << "  -p, --path    fersxml file path (required)\n"
-              << "  -v, --verbose    run with verbose outputs (optional)\n";
+              << "  -h, --help      Show this help message\n"
+              << "  -p, --path      fersxml file path (required)\n"
+              << "  -s, --schema    fersxml.xsd schema location (requried)"
+              << "  -v, --verbose   run with verbose outputs (optional)\n";
 }
 
 // Error handler that catches validation errors and further provides information about the errors.
@@ -78,6 +79,7 @@ int main(int argc, char *argv[])
     try
     {
         std::string strFilePath = "";
+        std::string strFerXMLSchemaPath = "";
         bool bRunAsVerbose = false;
 
         // Parse command-line arguments
@@ -89,9 +91,15 @@ int main(int argc, char *argv[])
                 printUsage(argv[0]);
                 return 0;
             }
-            else if (arg == "-p" || arg == "--path")
+            else if (arg == "-c" || arg == "--config")
             {
                 strFilePath = argv[++i];
+                std::cout << "ferxml path set to: " << strFilePath << std::endl;
+            }
+            else if (arg == "-s" || arg == "--schema")
+            {
+                strFerXMLSchemaPath = argv[++i];
+                std::cout << "ferxml xsd path set to: " << strFerXMLSchemaPath << std::endl;
             }
             else if (arg == "-v" || arg == "--verbose")
             {
@@ -103,6 +111,11 @@ int main(int argc, char *argv[])
         if (strFilePath.empty())
         {
             std::cerr << "Error: no fersxml file given, run with -h\n";
+            return 1;
+        }
+        if (strFerXMLSchemaPath.empty())
+        {
+            std::cerr << "Error: no fersxml.xsd schema file given, run with -h\n";
             return 1;
         }
 
@@ -119,138 +132,138 @@ int main(int argc, char *argv[])
         parser.setDoSchema(true);
 
         // Set the custom XSD file for the parser by specifying the XSD file path and XSD file name
-        parser.setExternalNoNamespaceSchemaLocation("/Users/michaelaltshuler/Documents/5th Year/EEE4022F:Thesis/FERS Features/FERS Validator/FERS-schema/fers-xml.xsd");
+        std::cout << "Attempting to set xsd namespace: " + strFerXMLSchemaPath << std::endl;
+        parser.setExternalNoNamespaceSchemaLocation(strFerXMLSchemaPath.c_str());
+        std::cout << "Wohoo! Success!" << std::endl;
 
         // error handler instance created and set on the parser.
         MyErrorHandler errorHandler;
         parser.setErrorHandler(&errorHandler);
 
         // Establish a DOMDocument object and parse the input FERSXML file:
+        std::cout << "Attempting to parse" + strFilePath << std::endl;
         parser.parse(strFilePath.c_str());
-
-        cout << endl;
-
         if (parser.getErrorCount() == 0)
-        {
-
-            std::cout << "XML document is valid" << std::endl;
-
-            // Command to run xml_validator_output with mode and strFilePath as argument
-            std::string command = "./xml_validator_output " + strFilePath;
-            // Run the command using system() function
-            int result = std::system(command.c_str());
-
-            if (result == -1)
-            {
-                std::cerr << "Failed to run xml_validator_output." << std::endl;
-                return 1;
-            }
-
-            std::cout << endl;
-
-            // Prompting user whether they want to run the kml_visualiser
-            char run_kml_visualiser = 'n';
-            char custom_coordinates;
-            double referenceLatitude = -33.9545;
-            double referenceLongitude = 18.4563;
-            double referenceAltitude = 0;
-            std::string outputName;
-            std::string input;
-
-            std::cout << "Do you want to run the kml_visualiser program? (y/N): ";
-            std::getline(std::cin, input);
-
-            if (!input.empty())
-            {
-                run_kml_visualiser = input[0];
-            }
-
-            if (run_kml_visualiser == 'y' || run_kml_visualiser == 'Y')
-            {
-                std::cout << "Name a KML output file (defualt file name = file name of fersxml): ";
-                std::getline(std::cin, outputName);
-
-                if (outputName.empty())
-                {
-                    outputName = getFileNameFromPath(strFilePath);
-                }
-
-                std::cout << "Do you want to enter custom referenceLatitude, referenceLongitude, and referenceAltitude? (y/N): ";
-                input.clear();
-                std::getline(std::cin, input);
-                custom_coordinates = input.empty() ? 'n' : input[0];
-                std::cout << "Default coordinates set to University of Cape Town: <133.9577° S, 18.4612° E, 0>" << std::endl;
-
-                if (custom_coordinates == 'y' || custom_coordinates == 'Y')
-                {
-                    std::string input;
-
-                    std::cout << "Enter referenceLatitude (default: -33.9545): ";
-                    std::cin >> input;
-                    std::istringstream iss_latitude(input);
-                    if (!(iss_latitude >> referenceLatitude))
-                    {
-                        std::cerr << "Invalid input. Please enter a valid number for referenceLatitude." << std::endl;
-                        return 1;
-                    }
-
-                    std::cout << "Enter referenceLongitude (default: 18.4563): ";
-                    std::cin >> input;
-                    std::istringstream iss_longitude(input);
-                    if (!(iss_longitude >> referenceLongitude))
-                    {
-                        std::cerr << "Invalid input. Please enter a valid number for referenceLongitude." << std::endl;
-                        return 1;
-                    }
-
-                    std::cout << "Enter referenceAltitude (default: 0): ";
-                    std::cin >> input;
-                    std::istringstream iss_altitude(input);
-                    if (!(iss_altitude >> referenceAltitude))
-                    {
-                        std::cerr << "Invalid input. Please enter a valid number for referenceAltitude." << std::endl;
-                        return 1;
-                    }
-                }
-
-                std::string kml_visualiser_command = "./kml_visualiser";
-
-                if (custom_coordinates == 'y' || custom_coordinates == 'Y')
-                {
-                    kml_visualiser_command += " " + strFilePath + " " + outputName + ".kml" + " " + std::to_string(referenceLatitude) + " " + std::to_string(referenceLongitude) + " " + std::to_string(referenceAltitude);
-
-                    int kml_visualiser_result = std::system(kml_visualiser_command.c_str());
-
-                    if (kml_visualiser_result == -1)
-                    {
-                        std::cerr << "Failed to run kml_visualiser." << std::endl;
-                        return 1;
-                    }
-                }
-                else
-                {
-                    kml_visualiser_command += " " + strFilePath + " " + outputName + ".kml";
-                    int kml_visualiser_result = std::system(kml_visualiser_command.c_str());
-
-                    if (kml_visualiser_result == -1)
-                    {
-                        std::cerr << "Failed to run kml_visualiser." << std::endl;
-                        return 1;
-                    }
-                }
-
-                std::cout << outputName + ".kml" + " outputted to current working directory." << std::endl;
-            }
-            else if (run_kml_visualiser != 'n' && run_kml_visualiser != 'N')
-            {
-                std::cerr << "Invalid input. Please enter 'y' for yes or 'n' for no." << std::endl;
-                return 1;
-            }
-        }
-
+            std::cout << "Wohoo! Success!" + strFilePath << std::endl;
         else
         {
-            std::cout << "FERSXML invalid: Take note of given errors and warnings above." << std::endl;
+            // error should have been printed by parser
+            std::cout << "XML parsing encountered errors - see above" << std::endl;
+            return 1;
+        }
+
+        std::cout << "XML document is valid" << std::endl;
+
+        // Command to run xml_validator_output with mode and strFilePath as argument
+        std::string command = "./xml_validator_output " + strFilePath;
+        // Run the command using system() function
+        int result = std::system(command.c_str());
+
+        if (result == -1)
+        {
+            std::cerr << "Failed to run xml_validator_output." << std::endl;
+            return 1;
+        }
+
+        std::cout << endl;
+
+        // Prompting user whether they want to run the kml_visualiser
+        char run_kml_visualiser = 'n';
+        char custom_coordinates;
+        double referenceLatitude = -33.9545;
+        double referenceLongitude = 18.4563;
+        double referenceAltitude = 0;
+        std::string outputName;
+        std::string input;
+
+        std::cout << "Do you want to run the kml_visualiser program? (y/N): ";
+        std::getline(std::cin, input);
+
+        if (!input.empty())
+        {
+            run_kml_visualiser = input[0];
+        }
+
+        if (run_kml_visualiser == 'y' || run_kml_visualiser == 'Y')
+        {
+            std::cout << "Name a KML output file (defualt file name = file name of fersxml): ";
+            std::getline(std::cin, outputName);
+
+            if (outputName.empty())
+            {
+                outputName = getFileNameFromPath(strFilePath);
+            }
+
+            std::cout << "Do you want to enter custom referenceLatitude, referenceLongitude, and referenceAltitude? (y/N): ";
+            input.clear();
+            std::getline(std::cin, input);
+            custom_coordinates = input.empty() ? 'n' : input[0];
+            std::cout << "Default coordinates set to University of Cape Town: <133.9577° S, 18.4612° E, 0>" << std::endl;
+
+            if (custom_coordinates == 'y' || custom_coordinates == 'Y')
+            {
+                std::string input;
+
+                std::cout << "Enter referenceLatitude (default: -33.9545): ";
+                std::cin >> input;
+                std::istringstream iss_latitude(input);
+                if (!(iss_latitude >> referenceLatitude))
+                {
+                    std::cerr << "Invalid input. Please enter a valid number for referenceLatitude." << std::endl;
+                    return 1;
+                }
+
+                std::cout << "Enter referenceLongitude (default: 18.4563): ";
+                std::cin >> input;
+                std::istringstream iss_longitude(input);
+                if (!(iss_longitude >> referenceLongitude))
+                {
+                    std::cerr << "Invalid input. Please enter a valid number for referenceLongitude." << std::endl;
+                    return 1;
+                }
+
+                std::cout << "Enter referenceAltitude (default: 0): ";
+                std::cin >> input;
+                std::istringstream iss_altitude(input);
+                if (!(iss_altitude >> referenceAltitude))
+                {
+                    std::cerr << "Invalid input. Please enter a valid number for referenceAltitude." << std::endl;
+                    return 1;
+                }
+            }
+
+            std::string kml_visualiser_command = "./kml_visualiser";
+
+            if (custom_coordinates == 'y' || custom_coordinates == 'Y')
+            {
+                kml_visualiser_command += " " + strFilePath + " " + outputName + ".kml" + " " + std::to_string(referenceLatitude) + " " + std::to_string(referenceLongitude) + " " + std::to_string(referenceAltitude);
+
+                int kml_visualiser_result = std::system(kml_visualiser_command.c_str());
+
+                if (kml_visualiser_result == -1)
+                {
+                    std::cerr << "Failed to run kml_visualiser." << std::endl;
+                    return 1;
+                }
+            }
+            else
+            {
+                kml_visualiser_command += " " + strFilePath + " " + outputName + ".kml";
+                int kml_visualiser_result = std::system(kml_visualiser_command.c_str());
+
+                if (kml_visualiser_result == -1)
+                {
+                    std::cerr << "Failed to run kml_visualiser." << std::endl;
+                    return 1;
+                }
+            }
+
+            std::cout << outputName + ".kml" + " outputted to current working directory." << std::endl;
+        }
+        else if (run_kml_visualiser != 'n' && run_kml_visualiser != 'N')
+        {
+            std::cerr << "Invalid input. Please enter 'y' for yes or 'n' for no." << std::endl;
+            return 1;
         }
 
         // Function call deallocates memory associated with he parsed FERSXML document.
