debcb19217ca44dc7bf7d3a24e50b9015df950ec
David Young <davidsamuelyoung@protonmail.com>
2026-03-26 21:38:09 +0200

    build: enhance code coverage options and update lcov filtering
    Signed-off-by: David Young <davidsamuelyoung@protonmail.com>

--- run_tests_gen_coverage.sh
+++ run_tests_gen_coverage.sh
@@ -6,6 +6,6 @@ ctest --preset=coverage -j12
 
 cd build/coverage || exit 1
 
-lcov --capture --directory . --output-file coverage.info --ignore-errors source
-lcov --remove coverage.info '/usr/*' '*/_deps/*' '*/vcpkg_installed/*' '*/tests/*' --output-file coverage_filtered.info
+lcov --capture --directory . --output-file coverage.info --ignore-errors source,gcov,negative
+lcov --remove coverage.info '/usr/*' '*/tests/*' '*/_deps/*' '*/vcpkg_installed/*' --output-file coverage_filtered.info
 genhtml coverage_filtered.info --output-directory coverage_report --ignore-errors source
